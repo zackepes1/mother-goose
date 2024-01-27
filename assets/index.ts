efd84e6f2685f925595784cbaa99d0ab3176e62e
Zack Epes <zachary.epes@siemens.com>
2024-01-27 16:07:15 -0500

    added additional images

---
+++ assets/index.ts
@@ -0,0 +1,21 @@
+import logo from "@/assets/logo.svg";
+import hero from "@/assets/hero.jpg";
+import consultation from "@/assets/consultation.jpg";
+import support from "@/assets/support.jpg";
+import plan from "@/assets/plan.jpg";
+import baby from "@/assets/baby.jpeg";
+import blog from "@/assets/blog.jpg";
+import blog2 from "@/assets/blog2.jpg";
+import blog3 from "@/assets/blog3.jpg";
+
+export {
+consultation,
+support,
+plan,
+logo,
+hero,
+baby,
+blog,
+blog2,
+blog3
+};
