065ea7368c38cb351fd9197875faa2a7a631f8db
Zack Epes <zachary.epes@siemens.com>
2024-01-27 16:06:11 -0500

    updated image index to typescript and made images relative to assets

--- components/shared/Feature.tsx
+++ components/shared/Feature.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import Image from 'next/image'
-import { baby} from '@/assets'
+import { baby } from '@/assets'
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
