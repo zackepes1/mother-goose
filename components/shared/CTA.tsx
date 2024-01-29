4d9ec50dcf102dae2017b9453ddcb83c9cdf6b49
Zack Epes <zachary.epes@siemens.com>
2024-01-29 13:28:33 -0500

    updated CTA to gradient bg

--- components/shared/CTA.tsx
+++ components/shared/CTA.tsx
@@ -3,7 +3,7 @@ import { Button } from '../ui/button'
 
 const CTA = () => {
   return (
-    <div className="bg-primary p-10 xl:p-24 m-auto flex flex-col justify-center items-center">
+    <div className="bg-gradient-to-br from-pink-400 to-pink-600 p-10 xl:p-24 m-auto flex flex-col justify-center items-center">
       <h1 className="text-5xl items-center text-center p-5 lg:tracking-tighter lg:leading-12 font-raleway font-bold">Embrace Confidence, Nourish Your Bond, Embrace the Joy.</h1>
       <h2 className="text-2xl items-center text-center p-5 font-normal lg:tracking-tight font-raleway">It all starts with one click. Book your consultation and embark on a smooth breastfeeding journey with Mother Goose Lactation.</h2>
       <div className="flex flex-row items-center justify-center p-5">
